9f8719656ed826fb2a89ae938d211a03fb1ae500
vacmg <70143287+vacmg@users.noreply.github.com>
2025-03-15 21:44:28 +0100

    Format OSInterface

--- include/ESP_IDF_OSInterface.h
+++ include/ESP_IDF_OSInterface.h
@@ -2,36 +2,36 @@
 #include "esp_log.h"
 
 #ifdef OSInterfaceLogVerbose
-#undef OSInterfaceLogVerbose
+    #undef OSInterfaceLogVerbose
 #endif
 #define OSInterfaceLogVerbose(tag, format, ...) ESP_LOGV(tag, format, ##__VA_ARGS__)
 
 #ifdef OSInterfaceLogDebug
-#undef OSInterfaceLogDebug
+    #undef OSInterfaceLogDebug
 #endif
 #define OSInterfaceLogDebug(tag, format, ...) ESP_LOGD(tag, format, ##__VA_ARGS__)
 
 #ifdef OSInterfaceLogInfo
-#undef OSInterfaceLogInfo
+    #undef OSInterfaceLogInfo
 #endif
 #define OSInterfaceLogInfo(tag, format, ...) ESP_LOGI(tag, format, ##__VA_ARGS__)
 
 #ifdef OSInterfaceLogWarning
-#undef OSInterfaceLogWarning
+    #undef OSInterfaceLogWarning
 #endif
 #define OSInterfaceLogWarning(tag, format, ...) ESP_LOGW(tag, format, ##__VA_ARGS__)
 
 #ifdef OSInterfaceLogError
-#undef OSInterfaceLogError
+    #undef OSInterfaceLogError
 #endif
 #define OSInterfaceLogError(tag, format, ...) ESP_LOGE(tag, format, ##__VA_ARGS__)
 
 #ifdef OSInterfaceSetLogLevel
-#undef OSInterfaceSetLogLevel
+    #undef OSInterfaceSetLogLevel
 #endif
 #define OSInterfaceSetLogLevel(tag, level) esp_log_level_set(tag, level)
 
 #ifdef OSInterfaceGetLogLevel
-#undef OSInterfaceGetLogLevel
+    #undef OSInterfaceGetLogLevel
 #endif
 #define OSInterfaceGetLogLevel(tag) esp_log_get_level(tag)
