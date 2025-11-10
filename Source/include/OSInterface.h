2e992e161176347ac8c53c2c1df33f684dd75fef
vacmg <70143287+vacmg@users.noreply.github.com>
2025-11-10 23:20:34 +0100

    Add OSInterface_Queue

--- Source/include/OSInterface.h
+++ Source/include/OSInterface.h
@@ -6,6 +6,7 @@
 #include "OSInterface_BinarySemaphore.h"
 #include "OSInterface_Log.h"
 #include "OSInterface_Mutex.h"
+#include "OSInterface_Queue.h"
 #include "OSInterface_Timer.h"
 
 #define EXPAND_TO_STRING(x) #x
@@ -43,6 +44,7 @@ public:
      * @return OSInterface_Mutex* Pointer to the created mutex
      * @note The mutex is created in the unlocked state.
      * @note The mutex needs to be freed with delete.
+     * @note If there are any errors during the creation, nullptr is returned.
      */
     virtual OSInterface_Mutex* osCreateMutex() = 0;
 
@@ -52,6 +54,7 @@ public:
      * @return OSInterface_BinarySemaphore* Pointer to the created binary semaphore
      * @note The semaphore is created with the initial value of 0.
      * @note The semaphore needs to be freed with delete.
+     * @note If there are any errors during the creation, nullptr is returned.
      */
     virtual OSInterface_BinarySemaphore* osCreateBinarySemaphore() = 0;
 
@@ -64,6 +67,7 @@ public:
      * @param callbackArg Argument to pass to the callback function
      * @param timerName Name of the timer (can be nullptr)
      * @return OSInterface_Timer* Pointer to the created timer
+     * @note The timer needs to be freed with delete.
      * @note If there are any errors during the creation, nullptr is returned.
      *
      * @note **Callback Execution Context and Thread-Safety:**
@@ -79,6 +83,17 @@ public:
      */
     virtual OSInterface_Timer* osCreateTimer(uint32_t period, OSInterface_Timer::Mode mode, OSInterfaceProcess callback,
                                              void* callbackArg, const char* timerName) = 0;
+
+    /**
+     * @brief Create an inter-process, thread-safe message queue
+     *
+     * @param maxMessages Maximum number of messages in the queue
+     * @param messageSize Size of each message in bytes
+     * @return OSInterface_Queue* Pointer to the created queue
+     * @note The queue needs to be freed with delete.
+     * @note If there are any errors during the creation, nullptr is returned.
+     */
+    virtual OSInterface_Queue* osCreateQueue(uint32_t maxMessages, uint32_t messageSize) = 0;
 
     /**
      * @brief Allocate memory
---
+++ Source/include/OSInterface_Queue.h
@@ -0,0 +1,103 @@
+#ifndef OSINTERFACE_OSINTERFACE_MESSAGEQUEUE_H
+#define OSINTERFACE_OSINTERFACE_MESSAGEQUEUE_H
+
+#include <cstdint>
+
+class OSInterface_Queue
+{
+public:
+    virtual ~OSInterface_Queue() = default;
+
+    /**
+     * @brief Get the number of messages currently in the queue
+     *
+     * @return int Number of messages in the queue
+     */
+    virtual uint32_t length() = 0;
+
+    /**
+     * @brief Get the number of slots in the queue
+     *
+     * @return int Number of slots in the queue
+     */
+    virtual uint32_t size() = 0;
+
+    /**
+     * @brief Get the number of empty slots in the queue
+     *
+     * @return int Number of empty slots in the queue
+     */
+    virtual uint32_t available() = 0;
+
+    /**
+     * @brief Check if the queue is empty
+     *
+     * @return true if the queue is empty, false otherwise
+     */
+    virtual bool isEmpty() = 0;
+
+    /**
+     * @brief Check if the queue is full
+     *
+     * @return true if the queue is full, false otherwise
+     */
+    virtual bool isFull() = 0;
+
+    /**
+     * @brief Reset the queue, removing all messages
+     */
+    virtual void reset() = 0;
+
+    /**
+     * @brief Receive a message from the queue
+     *
+     * @param message Reference to store the received message
+     * @param maxTimeToWait_ms Maximum time to wait in milliseconds
+     * @return true if a message was received, false if the timeout was reached
+     */
+    virtual bool receive(void* message, uint32_t maxTimeToWait_ms) = 0;
+
+    /**
+     * @brief Receive a message from the queue from an ISR
+     *
+     * @param message Reference to store the received message
+     * @return true if a message was received, false otherwise
+     */
+    virtual bool receiveFromISR(void* message) = 0;
+
+    /**
+     * @brief Send a message to the back of the queue
+     *
+     * @param message Message to send
+     * @param maxTimeToWait_ms Maximum time to wait in milliseconds
+     * @return true if the message was sent, false if the timeout was reached
+     */
+    virtual bool sendToBack(const void* message, uint32_t maxTimeToWait_ms) = 0;
+
+    /**
+     * @brief Send a message to the back of the queue from an ISR
+     *
+     * @param message Message to send
+     * @return true if the message was sent, false otherwise
+     */
+    virtual bool sendToBackFromISR(const void* message) = 0;
+
+    /**
+     * @brief Send a message to the front of the queue
+     *
+     * @param message Message to send
+     * @param maxTimeToWait_ms Maximum time to wait in milliseconds
+     * @return true if the message was sent, false if the timeout was reached
+     */
+    virtual bool sendToFront(const void* message, uint32_t maxTimeToWait_ms) = 0;
+
+    /**
+     * @brief Send a message to the front of the queue from an ISR
+     *
+     * @param message Message to send
+     * @return true if the message was sent, false otherwise
+     */
+    virtual bool sendToFrontFromISR(const void* message) = 0;
+};
+
+#endif // OSINTERFACE_OSINTERFACE_MESSAGEQUEUE_H
